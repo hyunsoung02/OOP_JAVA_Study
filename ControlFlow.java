1cf48f199963e9589b137a405fa5580efeafe267
Kim Hyun Soung <hyhicoddu@gmail.com>
2026-06-17 22:53:31 +0900

    Update ControlFlow.java

--- ControlFlow.java
+++ ControlFlow.java
@@ -14,25 +14,25 @@ public class Test1 {
           
             int securityLevel = 5;
             if (securityLevel >= 4) {
-                System.out.println(" -> 🔐 보안 등급 확인 완료: 모든 메뉴 접근 가능");
+                System.out.println(" ->  보안 등급 확인 완료: 모든 메뉴 접근 가능");
             }
             
             // [switch문 특징] 조건식 결과가 int 범위 이하의 정수를 사용하어 loginPermission의 변수는 int형임. 
             // [if문과 switch문 비교] 모든 switch문은 이처럼 if문으로도 변경 가능합니다.
             switch (loginPermission) {
                 case 1: // [switch문 특징] case문 값으로 변수 불가, '1'이라는 상수만 가능
-                    System.out.println(" -> ⚙️ 관리자 설정 메뉴를 활성화합니다.");
+                    System.out.println(" ->  관리자 설정 메뉴를 활성화합니다.");
                     break; // [switch문 특징] break를 만나 switch문을 빠져나감 (없으면 다음 case까지 진행)
                 case 2:
-                    System.out.println(" -> 👤 일반 마이페이지를 활성화합니다.");
+                    System.out.println(" ->  일반 마이페이지를 활성화합니다.");
                     break;
                 default: // [switch문 특징] 일치하는 case가 없을 때 (생략 가능하지만 예외 처리를 위해 작성)
-                    System.out.println(" -> ❌ 알 수 없는 권한입니다.");
+                    System.out.println(" -> 알 수 없는 권한입니다.");
                     break;
             }
             
         } else {
-            System.out.println("🛑 비회원 상태입니다.");
+            System.out.println(" 비회원 상태입니다.");
         }
 
         System.out.println("\n--- [2] 반복문 및 중첩for문, 이름 붙은 반복문 검증 ---");
@@ -44,7 +44,7 @@ public class Test1 {
         FruitWarehouse: 
         for (int row = 1; row <= 3; row++) { // [for문 특징] 초기화, 조건식, 증감식으로 구성 (반복 횟수가 중요할 때)
             
-            System.out.println("📦 창고 " + row + "층 진열대를 점검합니다.");
+            System.out.println(" 창고 " + row + "층 진열대를 점검합니다.");
             
             // [중첩 for문] for문 안에 또 다른 for문을 포함 (횟수 제한 거의 없음)
             for (int col = 1; col <= 5; col++) {
@@ -60,17 +60,17 @@ public class Test1 {
                 
                 // [break문 특징] 자신이 포함된 하나의 반복문을 빠져나감 (보통 if문과 함께 사용)
                 if (appleStock == 0) {
-                    System.out.println("   🚨 [⚠️경고] 사과 재고가 모두 소진되어 이 층의 점검을 중단합니다.");
+                    System.out.println("    [경고] 사과 재고가 모두 소진되어 이 층의 점검을 중단합니다.");
                     break; // 이 break는 자기를 감싸고 있는 안쪽 for문 하나만 탈출합니다.
                 }
             }
             
-            System.out.println("⚠️ 현재 남은 바나나 재고: " + bananaStock + "개");
+            System.out.println(" 현재 남은 바나나 재고: " + bananaStock + "개");
             bananaStock--;
             
         
             if (bananaStock < 0) {
-                System.out.println("❌ 바나나 재고가 바닥나서 전체 창고 점검을 '통째로' 종료합니다.");
+                System.out.println(" 바나나 재고가 바닥나서 전체 창고 점검을 '통째로' 종료합니다.");
                 break FruitWarehouse; // 안쪽이 아니라 바깥쪽의 FruitWarehouse 반복문을 즉시 깨뜨리고 나감
             }
         }
